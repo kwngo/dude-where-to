6ee841137f477ed9acb4ed371e0b91681e3ed053
Kareem Kwong <kareem.kwong@gmail.com>
2019-04-13 13:35:59 -0400

    Add mapbox gl css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function Home() {
-  return <div>Welcome to Next.js!</div>;
+import Head from 'next/head';
+
+function IndexPage() {
+  return (
+     <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link href='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
 }
 
-export default Home;
+export default IndexPage;
